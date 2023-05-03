f8f3f742e9a348e81bab9bf533b99666de67e86a
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-03 11:36:47 +0700

    Update python-intro.py

--- python-intro.py
+++ python-intro.py
@@ -1,4 +1,11 @@
 #Hello World Python
-#Show the fisrt python output using Print Function
+#Show the first python output using Print Function
 print("Hello World")
 print("Hi Python")
+
+#Check version control of Python
+import sys
+print(sys.version)
+print(sys.winver)
+print(sys.gettrace)
+print(sys.argv)
