7fddeff298e8be6f7489b64f92e09cff1f771a22
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-03 13:53:49 +0700

    Update basic-data-types.py

--- basic-data-types.py
+++ basic-data-types.py
@@ -21,3 +21,17 @@ print(bool(1))
 print(bool(0))
 #the output : True
 #the output : False
+
+# Integer
+print(type(15))
+print(type(-24))
+print(type(0))
+print(type(1))
+# Float
+print(type(3.14))
+print(type(0.5))
+print(type(1.0))
+print(type(-5.0))
+# Boolean
+print(type(True))
+print(type(False))
