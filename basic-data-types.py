0f346018aad1883f35a6420baa7e4f453ebcf135
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-03 11:51:37 +0700

    Create basic-data-types.py

---
+++ basic-data-types.py
@@ -0,0 +1,15 @@
+#string
+print("Hello World")
+#integer
+print(50)
+#float
+print(2.77)
+#boolean
+print(True)
+print(False)
+
+#checking data type, we can assign that values to some variables
+a = "IT company"
+print(a)
+print(type(a))
+print(type(b))
