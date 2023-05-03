a57ddaf595bcb361149bde04827a89ea1b6031fd
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-03 11:40:08 +0700

    Update python-intro.py

--- python-intro.py
+++ python-intro.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 11:30:41 2023
+
+@author: ferisa.prestasi
+"""
+
 #Hello World Python
 #Show the first python output using Print Function
 print("Hello World")
