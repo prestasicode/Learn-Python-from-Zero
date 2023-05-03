9c89b46f55f141727ea3a5d99aea65bbd0a43ac4
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-03 16:55:02 +0700

    Update basic-data-types.py

--- basic-data-types.py
+++ basic-data-types.py
@@ -28,11 +28,17 @@ print(type(15))
 print(type(-24))
 print(type(0))
 print(type(1))
+
 # Float
 print(type(3.14))
 print(type(0.5))
 print(type(1.0))
 print(type(-5.0))
+
 # Boolean
 print(type(True))
 print(type(False))
+
+#to get more information about integer and float data
+print(sys.int_info)
+print(sys.float_info)
