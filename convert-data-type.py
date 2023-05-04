ca473cf3c2a59ebd77a28e19e2e23b9a5a491b5e
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-04 10:31:02 +0700

    Create convert-data-type.py

---
+++ convert-data-type.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 10:24:54 2023
+
+@author: ferisa.prestasi
+"""
+
+#Convert integer number to a string and float
+#assign variable number 
+
+number = 10
+#check data type of number, first
+print(type(number))
+#<class 'int'>
+
+#convert int to string using str() function
+#assign to new var conv1 and check type of data conv1
+conv1 = str(number)
+print(conv1)
+print(type(conv1))
+#<class 'str'>
+
+#convert int to float using float() function
+conv2 = float(number)
+print(conv2)
+print(type(conv2))
+#10.0
+#<class 'float'>
