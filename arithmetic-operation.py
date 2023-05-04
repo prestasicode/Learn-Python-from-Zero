6b080a5a2aa22164a1fec4d1ff20b8e3727eeea8
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-04 13:48:12 +0700

    Create arithmetic-operation.py

---
+++ arithmetic-operation.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 13:39:47 2023
+
+@author: ferisa.prestasi
+"""
+
+#addition
+data = 58+90+87+58+32+91.2+37.5
+print(data)
+print(type(data))
+#453.7
+#<class 'float'>
+
+#substraction
+data2 = 89-37-12.5-27
+print(data2)
+print(type(data2))
+#12.5
+#<class 'float'>
+
+#multiplication
+data3 = 76*7
+print(data3)
+print(type(data3))
+#532
+#<class 'int'>
+
+#division
+data4 = 700*89
+print(data4)
+print(type(data4))
+#62300
+#<class 'int'>
+
+#floor division
+data5 = 1200//35
+print(data5)
+print(type(data5))
+#34
+#<class 'int'>
+
+#floor division
+data5 = 1200//35
+print(data5)
+print(type(data5))
+#34
+#<class 'int'>
