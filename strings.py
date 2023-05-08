18c7c9b2d5d6732f73f901c38efb8a5d69cb8401
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-08 20:53:30 +0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,11 @@
 # Employ double quotation marks for describing a string
 "Hello World!"
+#"Hello World!"
 
 # Employ single quotation marks for describing a string
 'Hello World!'
+#'Hello World!'
+
+# Digitals and spaces in a string
+'3 6 9 2 6 8'
+#'3 6 9 2 6 8'
