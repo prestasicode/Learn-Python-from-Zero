660785d0214897a176d6e776aa5b3511e4c65acf
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-08 21:00:12 +0700

    Create slicing-strings.py

---
+++ slicing-strings.py
@@ -0,0 +1,10 @@
+data = "Hello World!'
+print(data)
+
+# Slicing on the variable 'message' with only index 0 to index 5
+data[0:5]
+#'Hello'
+
+# Slicing on the variable 'message' with only index 6 to index 12
+message[6:12]
+#'World!'
