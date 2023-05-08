5fc091e038649dfdf23cee0eba6dae281fa4757e
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-08 21:01:31 +0700

    Update slicing-strings.py

--- slicing-strings.py
+++ slicing-strings.py
@@ -6,5 +6,14 @@ data[0:5]
 #'Hello'
 
 # Slicing on the variable 'message' with only index 6 to index 12
-message[6:12]
+data[6:12]
 #'World!'
+
+# to select every second element in the variable 'message'
+data[::2]
+#'HloWrd'
+
+# corporation of slicing and striding
+# get every second element in range from index 0 to index 6
+data[0:6:2]
+#'Hlo'
