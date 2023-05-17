1d2df947d8bc82769a75e7975db56231688ecaaf
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-17 21:46:30 +0700

    Create string-operations.py

---
+++ string-operations.py
@@ -0,0 +1,19 @@
+#data in variable
+data = 'hello python!'
+print('Before uppercase: ', data )
+#output : Before uppercase: hello python!
+
+# convert uppercase the elements in a string
+data_upper = data.upper()
+print('After uppercase: ', data_upper)
+#output : After uppercase: HELLO PYTHON!
+
+# convert lowercase the elements in a string
+data_lower = data.lower()
+print('Again lowercase: ', data_lower)
+#output : Again lowercase: hello python!
+
+# convert first letter of string to uppercase
+data_title = data.title()
+print('The first element of the string is uppercase: ', data_title)
+#output : The first element of the string is uppercase: Hello Python!
