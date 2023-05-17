9a009af515e2692a7ac8d074e1535e9edcf8060c
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-05-17 21:51:57 +0700

    Update formating-strings.py

--- formating-strings.py
+++ formating-strings.py
@@ -5,12 +5,16 @@ The placeholder is defined using curly brackets: {}.
 """
 txt = "Hello {word}"
 print(txt.format(word = 'World!'))
+#output : Hello World!
 
 data1 = 'Hi, My name is {} and I am {} years old.'
 print(data1.format('Ferisa', 20))
+#output : Hi, My name is Ferisa and I am 20 years old.
 
 data2 = 'Hi, My name is {name} and I am {number} years old.'
 print(data2.format(name ='Ferisa', number = 20))
+#output : Hi, My name is Ferisa and I am 20 years old.
 
 data3 = 'Hi, My name is {0} and I am {1} years old.'
 print(data3.format('Ferisa', 20))
+#output : Hi, My name is Ferisa and I am 20 years old.
